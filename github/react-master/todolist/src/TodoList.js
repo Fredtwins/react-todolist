a31087594b6a8c228c859f18b633a6a42ba567f1
liangfapeng <Fredtwins@163.com>
2018-07-26 11:33:15 +0800

    新增

--- github/react-master/todolist/src/TodoList.js
+++ github/react-master/todolist/src/TodoList.js
@@ -5,7 +5,7 @@ class TodoList extends Component {
     // 一个类就肯定有一个constructor的构造函数，这是最优先执行的函数
     constructor(props) {
         super(props);
-        console.log(props);
+        // console.log(props);
         // 定义数据就定义在状态里面,相当于定义在vue的data里面
         this.state = {
             inputValue: '',
@@ -25,11 +25,19 @@ class TodoList extends Component {
                         onChange={this.handleInputChange.bind(this)}
                         placeholder={this.state.inputhloder}
                     />&nbsp;&nbsp;&nbsp;
-                <button>提交</button>
+                <button onClick={this.handelBtnClick.bind(this)}>提交</button>
                 </div>
                 <ul>
-                    <li>学英语</li>
-                    <li>learm React</li>
+                    {/*用map做循环，写一个函数  ,接收2个参数 item内容  index对应的索引*/}
+                    {
+                        this.state.list.map((item, index) => {
+                            {/*每一次循环都加上一个key值才不会报警告  */}
+                            return <li 
+                            key={index}
+                            onClick={this.handelItemDelete.bind(this, index)}
+                            >{item}</li>
+                        })
+                    }
                 </ul>
             </Fragment>
         )
@@ -48,6 +56,22 @@ class TodoList extends Component {
         this.setState({
             inputValue: e.target.value
         });
+    }
+
+    // 提交按钮点击事件
+    handelBtnClick() {
+        this.setState({
+            // ...是es6的扩展符，这样就可以展开在数组里面所有的东西
+            list: [...this.state.list, this.state.inputValue],
+            // 新增完后就清空input框里面的东西
+            inputValue: ''
+        })
+    }
+
+    // 点击删除
+    handelItemDelete(index) {
+        // 在绑定事件的时候传入他的index索引过来接收
+        console.log(index)
     }
 }
 //写完这些要在这里导入出去
